7eaca13c3ed81cc2505ba60c5f1ad2ecb586dcdc
geofannyher <geofannyher@gmail.com>
2024-11-12 19:22:00 +0700

    update speech

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import Star from "./pages/star";
-import SpeechTranscriber from "./pages/Test";
 
 function App() {
   return <Star />;
-  // return <SpeechTranscriber />;
 }
 
 export default App;
