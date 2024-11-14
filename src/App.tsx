cb52aa73b2942ae311e490a8c32c08994eebf7cb
geofannyher <geofannyher@gmail.com>
2024-11-14 13:34:46 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Star from "./pages/star";
 import StarVideo2 from "./pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Star />} />
-        <Route path="/video" element={<StarVideo2 />} />
+        {/* <Route index element={<Star />} /> */}
+        <Route index element={<StarVideo2 />} />
       </Routes>
     </BrowserRouter>
   );
