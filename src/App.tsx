9838905e31d8d90219a3b613b8f9f4e5c0c67878
geofannyher <geofannyher@gmail.com>
2024-11-14 13:19:48 +0700

    feat: add new model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Star from "./pages/star";
+import StarVideo2 from "./pages/Test";
 
 function App() {
-  return <Star />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Star />} />
+        <Route path="/video" element={<StarVideo2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
